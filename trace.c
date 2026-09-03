4c19fe77a4304096bd5d26f8932488de4932a394
Brady Brown <brbrou800@gmail.com>
2026-09-03 14:20:41 -0700

    Code structure complete. Need to verify functionality and check diffs

--- trace.c
+++ trace.c
@@ -1,25 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 #include <errno.h>
-#include <pcap/pcap.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netinet/ether.h>
+
+#include <pcap.h>
 
 #include "trace.h"
+#include "checksum.h"
 
 struct pcap_pkthdr;
 
+// Structs for the headers
+typedef struct Eth_header {
+    uint8_t dest_mac[6];
+    uint8_t src_mac[6];    
+    uint16_t ether_type;
+} Eth_header;
+
+typedef struct ARP_header {
+    uint8_t metadata[6];            // We dont use
+    uint16_t opcode;
+    uint8_t sender_mac[6];
+    uint8_t sender_ip[4];
+    uint8_t target_mac[6];
+    uint8_t target_ip[4];
+} ARP_header;
+
+typedef struct IPv4_header {
+    uint8_t ver_hl;
+    uint8_t tos;
+    uint8_t len_id_fragOffset[6];   // We dont use
+    uint8_t ttl;
+    uint8_t protocol;
+    uint16_t checksum;
+    uint8_t src_ip[4];
+    uint8_t dest_ip[4];
+} IPv4_header;
+
+typedef struct ICMP_header {
+    uint8_t type;
+    uint8_t code;                   // We dont use
+    uint16_t checksum;              // We dont print
+    uint16_t identifier;            // We dont use
+    uint16_t sequence_num;          // We dont use
+} ICMP_header;
+
+typedef struct TCP_header {
+    uint16_t src_port;
+    uint16_t dest_port;
+    uint32_t sequence_num;
+    uint32_t ack_num;
+    uint8_t data_offset_reserved;   // Data offset, reserved bits, and NS flag
+    uint8_t flags;                  // CWR, ECE, URG, ACK, PSH, RST, SYN, FIN
+    uint16_t window_size;
+    uint16_t checksum;
+    uint16_t urgent_pointer;        // We dont use
+} TCP_header;
+
+typedef struct UDP_header {
+    uint16_t src_port;
+    uint16_t dest_port;
+    uint16_t length;                // We dont print
+    uint16_t checksum;              // We dont print
+} UDP_header;
+
+
+
 int main(int argc, char *argv[])
 {
-    if (argc > 2)
-    {
+    if (argc > 2) {
         fprintf(stderr, "Too many parameters.");
-        return 1;
+        return EXIT_FAILURE;
     }
 
-    if (argc <= 1)
-    {
+    if (argc <= 1) {
         fprintf(stderr, "Too few parameters.");
-        return 1;
+        return EXIT_FAILURE;
     }
 
     const char *pcapFilename = argv[1];
@@ -30,47 +91,247 @@ int main(int argc, char *argv[])
 
     int result;
 
-    // pcap_t *pcap_open_offline(const char *fname, char *errbuf);
     pcap_t *handle = pcap_open_offline(pcapFilename, errbuf);
 
-    if (handle == NULL)
-    {
+    if (handle == NULL) {
         fprintf(stderr, "%s", errbuf);
         return 1;
     }
 
-    u_int64 packet_num = 0; 
-    
-    while ((result = pcap_next_ex(p, &pkt_header, &pkt_data)) >= 0)
+    u_int64 packet_num = 0;
+
+    while ((result = pcap_next_ex(handle, &pkt_header, &pkt_data)) >= 0)
     {
-        if (result == 0)
-        {
-            // Reading timeout
-            continue;
-        }
+        if (result == 0) {continue;}  // Reading timeout
         if (result == PCAP_ERROR_BREAK) {
             // printf("There is nothing left to read from the file.");
             break;
         }
-        
-        printf("Packet %lld has been received! Length: ", packet_num, pkt_header->len);
 
-        parse_ethernet()
+        printf("\n");
+        process_packets(packet_num, pkt_header, pkt_data);
+    }
 
+    if (result == PCAP_ERROR) {
+        // Error: Call pcap_geterr(p) to fetch the reason and exit/log.
+        pcap_close(handle);
+        return EXIT_FAILURE;
+    }
+    else if (result == PCAP_ERROR_BREAK) {
+        printf("Successfully reached the end of the file."); // TODO: Remove later
+        pcap_close(handle);
+        return EXIT_SUCCESS;
+    }
+
+    printf("Hmmm, we made it to the end of main."); // TODO: Remove later
+    pcap_close(handle);
+    return EXIT_FAILURE;
+}
+
+void process_packets(uint64_t packet_num, struct pcap_pkthdr *header, const u_char *data)
+{
+    printf("Packet number: %lld  Packet Len: %d", (unsigned long long)packet_num, header->len);
+
+    // Ethernet header: 1st 14 bytes
+    printf("\n\n\tEthernet Header");
+    const Eth_header *eth_header = (Eth_header *)data;
+    
+    struct ether_addr dest_mac;
+    struct ether_addr src_mac;
+    memcpy(&dest_mac, eth_header->dest_mac, sizeof(eth_header->dest_mac));
+    memcpy(&src_mac, eth_header->src_mac, sizeof(eth_header->src_mac));
+
+    printf("\n\t\tDest MAC: %s", ether_ntoa(&dest_mac));
+    printf("\n\t\tSource MAC: %s", ether_ntoa(&src_mac));
+
+    uint16_t ethernet_type = ntohs(eth_header->ether_type); 
+        // TODO: Assumes the header->caplen is >= 14
+    switch (ethernet_type)
+    {
+        case 0x0800: {
+            printf("\n\t\tType: IP");
+            printf("\n");
+            const IPv4_header *ip_header = (IPv4_header *)(data + sizeof(Eth_header));
+            parse_IP(ip_header, (data + sizeof(Eth_header)));
+            break;
+        }
+            
+        case 0x0806: {
+            printf("\n\t\tType: ARP");
+            printf("\n");
+            const ARP_header *arp_header = (ARP_header *)(data + sizeof(Eth_header));
+            parse_ARP(arp_header);
+            break;
+        }
+
+        default: {
+            printf("\n\t\tType: Unknown");  // TODO: Do i need Unknown PDU?
+            printf("\n");
+        }
 
     }
 
-    // if (result < 0) {
+   
 
-    // }
-
-    pcap_close(handle);
-    return 0;
 }
 
-int parse_ethernet() {
 
-
+void parse_IP(IPv4_header *ip_header, int len) {
+    // IP Header: Next 20 bytes
+    if (len < 20) {
+        printf("Unknown PDU\n");
+        return;
+    }
+    ip_header->ver_hl
+    printf("\n\tIP Header");
+    printf("\n\t\tTOS: 0x%x", ip_header->tos);
+    printf("\n\t\tTTL: %ld", ip_header->ttl);
+    printf("\n\t\tProtocol: ");
     
-    return 0;
+    uint8_t protocol = 0;
+
+    switch (ip_header->protocol) {
+        case 1: {
+            printf("ICMP");
+            protocol = 1;
+        }
+        case 6: {
+            printf("TCP");
+            protocol = 6;
+        }
+        case 17: {
+            printf("UDP");
+            protocol = 17;
+        }
+        default: {
+            printf("Unknown");
+            protocol = 0;
+        } 
+        printf("\n");
+    }
+
+
+    int16_t ck_sum = (ip_header->checksum[0] << 8 | ip_header->checksum[1])
+    if (in_cksum((unsigned short *)ip_header, (int)ip_header_len) == 0) {
+        printf("\n\t\tChecksum: Correct (0x%x)", ck_sum);
+    } 
+    else {
+        printf("\n\t\tChecksum: Incorrect (0x%x)", ck_sum);
+    }
+
+    struct in_addr ip_src;
+    struct in_addr ip_dest;
+    memcpy(&ip_src, ip_header->src_ip, sizeof(ip_header->src_ip));
+    memcpy(&ip_dest, ip_header->dest_ip, sizeof(ip_header->dest_ip));
+
+    printf("\n\t\tSender IP: %s", inet_ntoa(ip_src));
+    printf("\n\t\tDest IP: %s", inet_ntoa(ip_dest));
+    printf("\n");
+
+    switch (protocol) {
+        case 1: {
+            printf("\n\tICMP Header\n\t\tType: ");
+            switch (ntohs(ip_header + len)) {
+                case 0: printf("Reply");
+                case 8: printf("Request");
+                default: printf("Unknown");
+            }
+            break;
+        }
+        case 6: {
+            parse_TCP((TCP_header *)(ip_header + len));
+            break;
+        }
+        case 17: {
+            parse_UDP((UDP_header *)(ip_header + len));
+            break;
+        }
+        default: printf("Unknown");
+    }    
+}
+
+
+void parse_ARP(ARP_header *arp_header) {
+    printf("\n\tARP Request/Reply");
+    int opcode = ntohs(arp_header->opcode);
+    if (opcode == 1) {
+        printf("\n\t\tOpcode: Request");
+    }
+    else if (opcode == 2) {
+        printf("\n\t\tOpcode: Reply");
+    } 
+    else {
+        printf("\n\t\tOpcode: Unknown"); // SHOULD NEVER PRINT THIS
+    } 
+
+    struct ether_addr sender_mac;
+    struct ether_addr target_mac;
+    memcpy(&sender_mac, arp_header->sender_mac, sizeof(arp_header->sender_mac));
+    memcpy(&target_mac, arp_header->target_mac, sizeof(arp_header->target_mac));
+
+    struct in_addr ip_src;
+    struct in_addr ip_dest;
+    memcpy(&ip_src, arp_header->sender_ip, sizeof(arp_header->sender_ip));
+    memcpy(&ip_dest, arp_header->target_ip, sizeof(arp_header->target_ip));
+
+    printf("\n\t\tSender MAC: %s", ether_ntoa(&sender_mac));
+    printf("\n\t\tSender IP: %s", inet_ntoa(ip_src));
+    printf("\n\t\tTarget MAC: %s", ether_ntoa(&target_mac));
+    printf("\n\t\tTarget IP: %s", inet_ntoa(ip_dest));
+}
+
+void parse_TCP(TCP_header *tcp_header) {
+    // TCP Header: Next 20 bytes
+    printf("\n\tTCP Header");
+    printf("\n\t\tSource Port: ");
+    print_port(ntohs(tcp_header->src_port));
+    printf("\n\t\tDest Port: ");
+    print_port(ntohs(tcp_header->dest_port));
+    printf("\n\t\tSequence Number: %d", ntohs(tcp_header->sequence_num));
+    printf("\n\t\tAck Number: %d", ntohs(tcp_header->ack_num));
+    
+    if (tcp_header->flags & (1 << 6)) {printf("\n\t\tSYN Flag: Yes");}
+    else {printf("\n\t\tSYN Flag: No");}
+    if (tcp_header->flags & (1 << 5)) {printf("\n\t\tRST Flag: Yes");}
+    else {printf("\n\t\tRST Flag: No");}
+    if (tcp_header->flags & (1 << 7)) {printf("\n\t\tFIN Flag: Yes");}
+    else {printf("\n\t\tFIN Flag: No");}
+
+    printf("\n\t\tWindow Size: %d", tcp_header->window_size);
+    
+    uint16_t ck_sum = (tcp_header->checksum[0] << 8 | tcp_header->checksum[1])
+    if (in_cksum(ck_sum, 2) == 0) {
+        printf("\n\t\tChecksum: Correct (0x%4x)", ck_sum);
+    } 
+    else {
+        printf("\n\t\tChecksum: Incorrect (0x%4x)", ck_sum);
+    }
+    printf("\n");
+}
+
+void parse_UDP(UDP_header *udp_header) {
+    printf("\n\tUDP Header");
+}
+
+void print_port(uint16_t port) {
+    switch (port) {
+        case 80:
+            printf("HTTP");
+            break;
+        case 23:
+            printf("Telnet");
+            break;
+        case 21:
+            printf("FTP");
+            break;
+        case 110:
+            printf("POP3");
+            break;
+        case 25:
+            printf("SMTP");
+            break;
+        default:
+            printf("%u", (unsigned int)port);
+            break;
+    }
 }
